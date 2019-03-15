10ce4fed5ba40778a96a6f82b2f4881f73e1f1ef
camcom421 <cameron.winkley@gmail.com>
2019-03-14 20:27:12 -0600

    module completed

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
@@ -33,8 +34,8 @@ def close_connection(exception):
         connection.close()
 
 
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    jobs = execute_sql('Select job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
+    return render_template('index.html', jobs=jobs)
