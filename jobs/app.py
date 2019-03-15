34d3e170e841294d98c052661ff9c1d75c70ccb0
camcom421 <cameron.winkley@gmail.com>
2019-03-14 20:02:26 -0600

    module completed

--- jobs/app.py
+++ jobs/app.py
@@ -25,13 +25,14 @@ def execute_sql(sql, values=(), commit=False, single=False):
     cursor.close()
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
 
-                                                                                                                                                           'cb;4b/fc10'
+
 
 @app.route('/')
 @app.route('/jobs')
